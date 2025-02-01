096b75c2a7508f1aacb0f17695d8896737a66a7f
Minseo Kim <103576890+ddozakim@users.noreply.github.com>
2025-02-01 15:21:19 +0900

    open repository

---
+++ Source/EclipsedCaverns/EclipsedCaverns.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class EclipsedCaverns : ModuleRules
+{
+	public EclipsedCaverns(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/EclipsedCaverns/EclipsedCavernsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EclipsedCavernsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEclipsedCavernsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEclipsedCavernsGameMode();
+};
+
+
+
