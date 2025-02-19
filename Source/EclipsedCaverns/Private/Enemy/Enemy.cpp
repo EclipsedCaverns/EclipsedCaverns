cdb28c0dc83e0c88e270a5979d7bd9f4ff9e5fa0
Minseo Kim <103576890+ddozakim@users.noreply.github.com>
2025-02-19 16:21:23 +0900

    👽Enemy : 에셋 다운 및 FSM 구현

--- Source/EclipsedCaverns/Private/Enemy/Enemy.cpp
+++ Source/EclipsedCaverns/Private/Enemy/Enemy.cpp
@@ -2,30 +2,35 @@
 
 
 #include "Enemy/Enemy.h"
+#include "Enemy/EnemyFSM.h"
 
-// Sets default values
 AEnemy::AEnemy()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Asset/Creep/uploads_files_4240643_Creep.uploads_files_4240643_Creep'"));
+
+	if (tempMesh.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(tempMesh.Object);
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -88), FRotator(0, -90, 0));
+	}
+
+	fsm = CreateDefaultSubobject<UEnemyFSM>(TEXT("FSM"));
 }
 
-// Called when the game starts or when spawned
 void AEnemy::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void AEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
